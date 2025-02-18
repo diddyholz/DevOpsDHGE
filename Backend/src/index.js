c63be8670fc663785177aed46964c45b280134b9
Funkhi <pauljfunk@gmx.de>
2025-02-18 12:56:50 +0100

    Implemented simple file reading

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,26 @@
 import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import {v4 as uuidv4} from 'uuid';
+import { Console } from 'console';
+
+const configpath = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Preferences' : process.env.HOME + "/.config");
+const surveypath = path.join(configpath, 'surveys');
 
 const PORT = 12345;
 
 const app = express();
+
+const surveys = [];
+fs.mkdirSync(surveypath, { recursive: true });
+const dir = fs.opendirSync(surveypath);
+let dirent = dir.readSync();
+while (dirent) {
+  console.log(dirent.name);
+  const data = JSON.parse(fs.readFileSync(path.join(surveypath, dirent.name), 'utf8'));
+  surveys.push(data);
+  dirent = dir.readSync();
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -10,4 +28,8 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/survey', (req, res) => {
+  
 });
